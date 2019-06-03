119ed800ffd7e2ef0ca6e0356cd6220a16d5284c
Ryan Firman Santosa <gamerzzzmailing@gmail.com>
2019-06-03 12:38:01 +0700

    init push

---
+++ Submission1/Models/UserMudik.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Submission1.Models
+{
+    public class UserMudik
+    {
+        public string Nama { get; set; }
+        public string JobPosition { get; set; }
+        public bool IsMudik { get; set; }
+        public string Wilayah { get; set; }
+        public DateTime TanggalBerangkat { get; set; }
+        public UserMudik()
+        {
+
+        }
+
+        public UserMudik(string nama, string jobPosition, bool isMudik, string wilayah, DateTime tanggalBerangkat)
+        {
+            Nama = nama;
+            JobPosition = jobPosition;
+            IsMudik = isMudik;
+            Wilayah = wilayah;
+            TanggalBerangkat = tanggalBerangkat;
+        }
+    }
+}
